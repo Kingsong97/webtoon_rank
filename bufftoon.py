787b5c9952b6b54bb5a6cb547f90710a689f965f
Kingsong97 <161429740+Kingsong97@users.noreply.github.com>
2024-05-03 01:12:54 +0900

    Update bufftoon.py

--- bufftoon.py
+++ bufftoon.py
@@ -21,39 +21,34 @@ service = ChromeService(ChromeDriverManager().install())
 
 # 드라이버 초기화
 driver = webdriver.Chrome(service=service, options=options)
-# 현재 날짜 가져오기
-current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"bufftoon_{current_date}.json"
 
-# 웹 드라이브 설치
-options = ChromeOptions()
-service = ChromeService(executable_path=ChromeDriverManager().install())
-browser = webdriver.Chrome(service=service, options=options)
-browser.get("https://bufftoon.plaync.com/tag/ranking?currentType=webtoon")
+# 웹 드라이브를 설정하여 페이지에 접근
+driver.get("https://bufftoon.plaync.com/tag/ranking?currentType=webtoon")
 
 # 페이지가 완전히 로드될 때까지 대기
-WebDriverWait(browser, 10).until(
+WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "width-box"))
 )
 
 # 천천히 스크롤 다운
 scroll_pause_time = 1  # 1초 대기
 pixels_to_scroll = 200  # 한 번에 스크롤할 픽셀 수
-max_time_limit = 40  # 전체 작업 시간 제한 (60초)
+max_time_limit = 40  # 전체 작업 시간 제한 (40초)
 start_time = time.time()  # 작업 시작 시간
 
-def scroll_down():
-    """현재 위치에서 지정된 픽셀 수만큼 아래로 스크롤"""
-    browser.execute_script(f"window.scrollBy(0, {pixels_to_scroll});")
-
 while (time.time() - start_time) < max_time_limit:
-    scroll_down()
+    old_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script(f"window.scrollBy(0, {pixels_to_scroll});")
     time.sleep(scroll_pause_time)
-    new_height = browser.execute_script("return document.body.scrollHeight")
-    if new_height == browser.execute_script("return window.pageYOffset + window.innerHeight"):
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == old_height:
         break  # 페이지 끝에 도달
 
-html_source_updated = browser.page_source
+# 현재 날짜 가져오기
+current_date = datetime.now().strftime("%Y-%m-%d")
+filename = f"bufftoon_{current_date}.json"
+
+html_source_updated = driver.page_source
 soup = BeautifulSoup(html_source_updated, 'html.parser')
 
 # 데이터 추출
@@ -76,4 +71,4 @@ with open(filename, 'w', encoding='utf-8') as f:
     json.dump(webtoon_data, f, ensure_ascii=False, indent=4)
 
 # 브라우저 종료
-browser.quit()
+driver.quit()
